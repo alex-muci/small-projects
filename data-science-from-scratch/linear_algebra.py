f700765815c8d63fec5735b6e2827fd8d4455f7f
Alex Muci <alex.muci@gmail.com>
2019-08-06 18:02:43 +0100

    typoo

--- data-science-from-scratch/linear_algebra.py
+++ data-science-from-scratch/linear_algebra.py
@@ -4,10 +4,16 @@ from functools import reduce
 
 from typing import List, Tuple, Callable
 
+"""
+    vectors
+"""
 Vector = List[float]
 
 def vector_add(v: Vector, w: Vector) -> Vector:
     assert len(v) == len(w)
     return [vi + wi for vi, wi in zip(v, w)]
 
-assert vector_add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
+
+if __name__ == "__main__":
+
+    assert vector_add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
